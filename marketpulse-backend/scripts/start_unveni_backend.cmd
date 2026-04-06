b533fbecfbea0ff1f76c392b2da3b950327ed53c
Mustafa <mustafa2002f@gmail.com>
2026-04-06 16:09:23 -0500

    Remove all repository contents

--- marketpulse-backend/scripts/start_unveni_backend.cmd
+++
@@ -1,2 +0,0 @@
-@echo off
-powershell.exe -NoProfile -WindowStyle Hidden -ExecutionPolicy Bypass -File "C:\Users\musta\OneDrive\Desktop\MarketPulse-main\marketpulse-backend\scripts\start_unveni_backend.ps1"
